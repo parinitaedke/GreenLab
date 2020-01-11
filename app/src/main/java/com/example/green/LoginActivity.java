f3258b9565339e3b06c29e88bae3b897b1235fdd
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2020-01-11 16:41:06 -0500

    Added the auto  generation of the database for the fireBAse

--- app/src/main/java/com/example/green/LoginActivity.java
+++ app/src/main/java/com/example/green/LoginActivity.java
@@ -16,14 +16,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText emailEditText;
     private EditText passwordEditText;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance("https://hackathon-ffdbf.firebaseio.com/");
         System.out.println("this is the login Activity");
         setContentView(R.layout.activity_login);
 
@@ -52,7 +56,6 @@ public class LoginActivity extends AppCompatActivity {
 
         // This is setting up the firebaseDatabase and it inputs message as the key and Hello world
         // as the value.
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
         findViewById(R.id.sign_up).setOnClickListener(new View.OnClickListener() {
@@ -71,6 +74,11 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            // getUid will never be null since it will only enter if it has registered a new user
+                            DatabaseReference myRef = database.getReference(mAuth.getCurrentUser().getUid());
+                            HashMap<String, Object> myMap = new HashMap<String, Object>();
+                            myMap.put("Balance","0");
+                            myRef.updateChildren(myMap);
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
