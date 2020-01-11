31b15e9ec758c89880df86fdb372b2d02f05b4ec
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2020-01-11 16:07:50 -0500

    Finished the login and registration

--- app/src/main/java/com/example/green/LoginActivity.java
+++ app/src/main/java/com/example/green/LoginActivity.java
@@ -1,24 +1,59 @@
 package com.example.green;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("this is the login Activity");
         setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        System.out.println(emailEditText.getText());
+
+        // Authentication commencement.
+        mAuth = FirebaseAuth.getInstance();
+
+        // This is for registration.
+        findViewById(R.id.sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.register();
+            }
+        });
+
+        //This is the the login
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.login();
+            }
+        });
+
+        // This is setting up the firebaseDatabase and it inputs message as the key and Hello world
+        // as the value.
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
-
         myRef.setValue("Hello, World!");
         findViewById(R.id.sign_up).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,8 +62,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
     private void register(){
-        Intent newIntent = new Intent(this, Register.class);
-        startActivity(newIntent);
+        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void login(){
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
     }
 }
