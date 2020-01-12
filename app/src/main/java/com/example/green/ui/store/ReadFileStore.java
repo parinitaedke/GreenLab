13e0eac128f79d6156360bc4bd8176c917f4ab18
parinitaedke <parinita.u.edke@hotmail.com>
2020-01-12 00:37:42 -0500

    Added the ReadFileStore class and stores.csv file. Also changed the delimiter to a semi-colon instead of comma

--- app/src/main/java/com/example/green/ui/store/ReadFileStore.java
+++ app/src/main/java/com/example/green/ui/store/ReadFileStore.java
@@ -1,21 +1,21 @@
-package com.example.green.ui.events;
+package com.example.green.ui.store;
 
-import com.example.green.Event;
+import com.example.green.Store;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ReadExcelFileDemo {
-    private static ArrayList<String> date = new ArrayList<String>();
-    private static ArrayList<String> time = new ArrayList<String>();
+public class ReadFileStore {
     private static ArrayList<String> name = new ArrayList<String>();
     private static ArrayList<String> location = new ArrayList<String>();
+    private static ArrayList<String> category = new ArrayList<String>();
+    private static ArrayList<String> contacts = new ArrayList<String>();
 
     public static void main(String[] args) {
         //Input file which needs to be parsed
-        String fileToParse = "/Users/parinitaedke/Desktop/SheHacks/app/src/main/java/com/example/green/ui/events/events.csv";
+        String fileToParse = "/Users/parinitaedke/Desktop/SheHacks/app/src/main/java/com/example/green/ui/store/stores.csv";
         BufferedReader fileReader = null;
 
         //Delimiter used in CSV file
@@ -32,9 +32,9 @@ public class ReadExcelFileDemo {
                 //Get all tokens available in line
                 String[] tokens = line.split(DELIMITER);
                 name.add(tokens[0]);
-                date.add(tokens[1]);
-                time.add(tokens[2]);
-                location.add(tokens[3]);
+                location.add(tokens[1]);
+                category.add(tokens[2]);
+                contacts.add(tokens[3]);
 
             }
         } catch (Exception e) {
@@ -48,13 +48,13 @@ public class ReadExcelFileDemo {
         }
     }
 
-    public ArrayList<Event> createEvents() {
-        ArrayList<Event> events = new ArrayList<Event>();
+    public ArrayList<Store> createStores() {
+        ArrayList<Store> stores = new ArrayList<Store>();
         for (int i = 0; i < this.name.size(); i++ ){
-            Event newEvent = new Event(this.date.get(i), this.time.get(i), this.name.get(i), this.location.get(i));
-            events.add(newEvent);
+            Store newStore = new Store(this.name.get(i), this.location.get(i), this.category.get(i), this.contacts.get(i));
+            stores.add(newStore);
         }
-        return events;
+        return stores;
 
     }
 }
