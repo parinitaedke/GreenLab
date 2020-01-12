1b3c799c1283b92596eda0a21dc38adcf3f47da2
choykwan <kwan.choy@mail.utoronto.ca>
2020-01-11 23:39:45 -0500

    store location comparator

--- app/src/main/java/com/example/green/StoreLocationComparator.java
+++ app/src/main/java/com/example/green/StoreLocationComparator.java
@@ -1,4 +1,35 @@
 package com.example.green;
 
-public class StoreLocationComparator  {
+import android.location.Location;
+
+import java.util.Comparator;
+
+public class StoreLocationComparator  implements Comparator<Location> {
+    Location currentLoc;
+
+    public StoreLocationComparator(Location current){
+        currentLoc = current;
+    }
+    @Override
+    public int compare(final Location place1, final Location place2) {
+        double lat1 = place1.getLatitude();
+        double lon1 = place1.getLongitude();
+        double lat2 = place2.getLatitude();
+        double lon2 = place2.latlng.longitude;
+
+        double distanceToPlace1 = distance(currentLoc.latitude, currentLoc.longitude, lat1, lon1);
+        double distanceToPlace2 = distance(currentLoc.latitude, currentLoc.longitude, lat2, lon2);
+        return (int) (distanceToPlace1 - distanceToPlace2);
+    }
+
+    public double distance(double fromLat, double fromLon, double toLat, double toLon) {
+        double radius = 6378137;   // approximate Earth radius, *in meters*
+        double deltaLat = toLat - fromLat;
+        double deltaLon = toLon - fromLon;
+        double angle = 2 * Math.asin( Math.sqrt(
+                Math.pow(Math.sin(deltaLat/2), 2) +
+                        Math.cos(fromLat) * Math.cos(toLat) *
+                                Math.pow(Math.sin(deltaLon/2), 2) ) );
+        return radius * angle;
+    }
 }
